72355edb5fe1d37b68e66e5e63566b07644616a4
bricso1 <debrunarthur3@gmail.com>
2024-06-19 13:56:39 +0200

    update game.py
    add s a modular system for all try

--- game.py
+++ game.py
@@ -1,17 +1,24 @@
 from random import randint
-a=randint(1,100)
+r1=int(input("jusqu'à combien voulez vous cherchez ?"))
+try1=int(input("combien d'essaie voulez-vous faire ? "))
+try2=0
+a=randint(1,r1)
 b=0
 while True:
-    while b!=a:
-        b=int(input("Choisez un nombre entre 1 et 100 : "))
+    while b!=a & try1<=try2:
+        b=int(input("Choisez un nombre entre 1 et ",r1," : "))
+        try2=tyr2+1
         if b>a:
             print("Ton nombre est trop haut. Réessaye")
         elif b<a:
             print("Ton nombre est trop bas. Réssaye")
+    if try1>try2:
+        print("t'as ton compte d'essai")
+    else:
         print("Bravo tu as trouvé le bon nombre")
     c=input("Rejouez ? ")
     if c==True :
-        print("Continuez")
+        print("On continue")
     else:
         break
 print("C'est fini!")
